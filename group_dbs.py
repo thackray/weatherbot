bd8fa57078756a721cc411da8f554dc7af17b5f6
thackray <thackray@mit.edu>
2015-10-14 13:16:37 -0400

    finally lined up data frames

--- group_dbs.py
+++ group_dbs.py
@@ -7,10 +7,9 @@ station = sys.argv[1]
 D = pd.DataFrame.from_csv('data/%s_NAM.csv'%station)
 D2 = pd.DataFrame.from_csv('data/%s_GFS.csv'%station)
 D3 = pd.DataFrame.from_csv('data/%s_OBS.csv'%station)
-D.index = [dd[:10] for dd in D['fdate']]
-D2.index = [dd[:10] for dd in D2['fdate']]
-D3['fdate'] = [str(dd)[:10] for dd in D3.index]
-D3.index = [dd[:10] for dd in D3['fdate']]
+D['fdate'] = [dd[:10] for dd in D['fdate']]
+D2['fdate'] = [dd[:10] for dd in D2['fdate']]
+#D3.index = [dd[:10] for dd in D3['fdate']]
 
 #print D2
 #D.merge(D2,on='fdate', how='outer')
--- process_obs.py
+++ process_obs.py
@@ -4,7 +4,9 @@ import os
 
 wban = 14739 #bos
 station = 'KBOS'
+iii = 0
 def read_raw_obs(obsfile,wban=14739,station='KBOS'):
+    global iii
     dbfile = 'data/%s_OBS.csv'%station
     if os.path.exists(dbfile):
         db = pd.DataFrame.from_csv(dbfile, index_col=0)
@@ -14,23 +16,28 @@ def read_raw_obs(obsfile,wban=14739,station='KBOS'):
     with open(obsfile,'r') as f:
         lines = f.readlines()
         head = lines[0]
-        keepers = [head]
+        keepers = [',fdate,OBS_maxwind,OBS_Tmax,OBS_Tmin,OBS_precip\n']
         for line in lines[1:]:
-            if line.startswith(str(wban)):
-                keepers.append(line)
+            ll = line.split(',')
+            if ll[0] in [str(wban)]:
+                fdate = ll[1][:4]+'-'+ll[1][4:6]+'-'+ll[1][6:]
+                keepers.append(','.join([str(iii),fdate,ll[-4],
+                                         ll[2],ll[4],ll[30]])+'\n')
+                iii += 1
     with open('temp','w') as f:
         for line in keepers:
             f.write(line)
 
-    newdb = pd.DataFrame.from_csv('temp', index_col=1)
+    newdb = pd.DataFrame.from_csv('temp', index_col=0)
     
     outdb = pd.concat([db,newdb])
 #    print outdb
     outdb.to_csv(dbfile)
     return
 
-
-for eachfile in os.listdir('data/obs'):
+dircont = os.listdir('data/obs')
+dircont.sort()
+for eachfile in dircont:
     if eachfile.endswith('daily.txt'):
         print eachfile
         read_raw_obs('data/obs/%s'%eachfile, wban=wban,station=station)
