a835276f6347acc034b24d5208a605a35b6bb3be
thackray <thackray@mit.edu>
2015-10-16 16:50:33 -0400

    added weighting estimator

--- data_diags.py
+++ data_diags.py
@@ -11,6 +11,16 @@ dbfile = 'data/%s.db'%station
 
 db = pd.read_csv(dbfile, index_col=0, na_values='')
 
+mask = np.isnan(db['OBS_Tmax'])+np.isnan(db['OBS_Tmin'])
+#mask = np.logical_not(mask)
+arr = np.where(mask)
+db['GFS_diratmax'] = np.cos(db['GFS_diratmax']*np.pi/180.)
+db['NAM_diratmax'] = np.cos(db['NAM_diratmax']*np.pi/180.)
+
+#print arr
+for aa in arr:
+    db = db.drop(aa)
+
 def scat(xx,yy,newfig=True):
     if newfig:
         pl.figure()
@@ -59,24 +69,75 @@ def get_nearest_n(db, vals, fields, weights, n=100):
     dist = calc_dist(db, vals, fields, weights)
     return dist.argsort()[:n].values
 
+def get_nearest_n_plus(db, vals, fields, weights, n=100):
+    dist = calc_dist(db, vals, fields, weights)
+    args = dist.argsort()[:n].values
+    return args, dist[args]
+
+
 def get_field_near(db, near, field):
     return db[field][near].values
 
-#three_panel('Tmax')
-#three_panel('Tmin')
+def get_weighted_estimate(db, field, vals, preds, weights, n=100):
+    near, dists = get_nearest_n_plus(db, vals, preds, weights, n=n)
+    obs = get_field_near(db, near, field)
+    est1 = obs*np.exp(-dists)
+    est = np.sum(est1)/np.sum(np.exp(-dists))
+    return est
 
-err = []
-for day, true in enumerate(db['OBS_Tmax']):
-    near = get_nearest_n(db, 
-                         [db['GFS_Tmax'][day],db['NAM_Tmax'][day]],
-                         ['GFS_Tmax','NAM_Tmax'], 
-                         [1.,1.], n=100)
-    obs = get_field_near(db, near, 'OBS_Tmax')
-    err.append(abs(np.median(obs) - true))
+def get_median_estimate(db, field, vals, preds, weights, n=100):
+    near = get_nearest_n(db, vals, preds, weights, n=n)
+    obs = get_field_near(db, near, field)
+    return np.median(obs)
 
-print err
-                         
-pl.figure()
-pl.hist(err)
+def get_hist(db, truth, predictors, weights, n=30):
+    err = []
+    for day, true in zip(db.index,db[truth]):
+        vals = [db[p][day] for p in predictors]
+        est = get_median_estimate(db, truth, vals, predictors, weights, n=n)
+        err.append(abs(est - true))
+    pl.figure()
+    pl.title(' '.join(predictors))
+    pl.hist(err, bins=range(0,16))
+
+def get_hist_weighted(db, truth, predictors, weights, n=30):
+    err = []
+    for day, true in zip(db.index,db[truth]):
+        vals = [db[p][day] for p in predictors]
+        est = get_weighted_estimate(db, truth, vals, predictors, weights, n=n)
+        err.append(abs(est - true))
+    pl.figure()
+    pl.title(' '.join(predictors))
+    pl.hist(err, bins=range(0,16))
+    return err
+
+
+three_panel('Tmax')
+three_panel('Tmin')
+
+get_hist_weighted(db, 'OBS_Tmin', 
+         ['GFS_Tmin','NAM_Tmin', 'GFS_windatmin', 'NAM_windatmin'],
+         [1.,1.,0.5,0.5])
+
+errr = get_hist_weighted(db, 'OBS_Tmin', 
+         ['GFS_Tmin','NAM_Tmin'],
+         [1.,1.])
+
+get_hist_weighted(db, 'OBS_Tmin', 
+         ['GFS_Tmin','NAM_Tmin', 'GFS_windatmin', 'NAM_windatmin',
+          'GFS_dptatmin', 'NAM_dptatmin'],
+         [1.,1.,0.5,0.5,1.,1.])
+
+get_hist_weighted(db, 'OBS_Tmin', 
+                  ['GFS_Tmin','NAM_Tmin', 'GFS_dptatmin', 'NAM_dptatmin'],
+                  [1.,1.,.5,.5])
+
+get_hist_weighted(db, 'OBS_Tmin', 
+                  ['GFS_Tmin','NAM_Tmin','GFS_Tmax','NAM_Tmax'],
+                  [1.,1.,.5,.5])
+
+get_hist_weighted(db, 'OBS_Tmax', 
+         ['GFS_Tmax','NAM_Tmax'],
+         [1.,1.])
 
 pl.show()
