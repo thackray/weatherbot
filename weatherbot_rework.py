3406cdfbf1e53429a13611f9ac0f4f777ed26abd
thackray <thackray@mit.edu>
2015-12-04 17:16:57 -0500

    starting rework

---
+++ weatherbot_rework.py
@@ -0,0 +1,14 @@
+class Weatherbot(object):
+    def __init__(self):
+        pass
+    def deliver_forecast(self):
+        pass
+    def estimate_quantity(self,field):
+        pass
+    def set_station(self, station):
+        pass
+    def get_config(self, configfile):
+        """read parameters for estimation from file
+        include what should be forecast and weights etc. to use"""
+        pass
+    
