77dc850a28d4259d496d1641f8956382afc63eba
thackray <thackray@mit.edu>
2015-12-04 17:29:46 -0500

    starting rework

--- weatherbot_rework.py
+++ weatherbot_rework.py
@@ -1,14 +1,30 @@
+Tmax_config = {'confidence_intervals':[2.,0.2,0.02],
+               'predictors':['NAM_Tmax','NAM_cloudcatatmax'],
+               'weights':[1.,999.],
+               }
+total_config = {'forecast_quantities':['Tmax'],
+                'Tmax':Tmax_config,
+                }
+
 class Weatherbot(object):
-    def __init__(self):
-        pass
+    def __init__(self, station=None,configfile=None,configdict=None):
+        self.station = station
+        self.configfile = configfile
+        self.configdict = configdict
+        return
     def deliver_forecast(self):
         pass
     def estimate_quantity(self,field):
-        pass
+        return estimate, variance, confidence
     def set_station(self, station):
-        pass
-    def get_config(self, configfile):
+        self.station = station
+                
+    def get_config(self, configfile=None):
         """read parameters for estimation from file
-        include what should be forecast and weights etc. to use"""
+        include what should be forecast and weights etc. to use
+        parameters for each quantity predicted:
+        - confidence levels: e.g. [2,0.2,0.02]
+        - predictors: e.g. [Tmax, cloudcategory, datecategory]
+        - weights: e.g. [1,999,999]"""
         pass
     
